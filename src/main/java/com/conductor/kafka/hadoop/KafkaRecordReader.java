f6e173288588f9690fa286c7dfe25f81ca5193c2
Greg Temchenko <soid.exe@gmail.com>
2015-10-08 17:29:37 -0700

    kafka 0.8 compatibility; new integration test added using kafka-unit; still need to fix offsets managment
    Conflicts:
    	pom.xml

--- src/main/java/com/conductor/kafka/hadoop/KafkaRecordReader.java
+++ src/main/java/com/conductor/kafka/hadoop/KafkaRecordReader.java
@@ -89,7 +89,6 @@ public class KafkaRecordReader extends RecordReader<LongWritable, KafkaMessageWi
         this.split = inputSplit;
         this.start = inputSplit.getStartOffset();
         this.pos = inputSplit.getStartOffset();
-        this.currentOffset = inputSplit.getStartOffset();
         this.end = inputSplit.getEndOffset();
         this.fetchSize = KafkaInputFormat.getKafkaFetchSizeBytes(conf);
         this.consumer = getConsumer(inputSplit, conf);
